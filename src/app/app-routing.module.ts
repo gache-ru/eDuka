a7ad1d0dd542b4c1b19f29f50184ec206ec14acf
FGacheru <gacheruf12@gmail.com>
2023-09-04 12:16:01 +0300

    Add a navbar with a dropdown for ngbModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ProductsComponent } from './layout/products/products.component';
+import { ShoppingCartComponent } from './layout/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './layout/check-out/check-out.component';
+import { OrderSuccessComponent } from './layout/order-success/order-success.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
- {
-  path:'',
-  component: NavBarComponent
- }
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'shopping-cart',
+    component: ShoppingCartComponent
+  },
+  {
+    path: 'check-out',
+    component: CheckOutComponent
+  },
+  {
+    path: 'order-success',
+    component: OrderSuccessComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'admin/product',
+    component: AdminProductsComponent
+  },
+  {
+    path: 'admin/orders',
+    component: AdminOrdersComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,18 +11,37 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ProductsComponent } from './layout/products/products.component';
+import { ShoppingCartComponent } from './layout/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './layout/check-out/check-out.component';
+import { OrderSuccessComponent } from './layout/order-success/order-success.component';
+import { MyOrdersComponent } from './layout/my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
