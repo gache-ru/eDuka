df9077284a6652fd09510c1aa3bc431dce344bac
FGacheru <gacheruf12@gmail.com>
2025-01-21 19:32:08 +0300

    Enable the admin auth guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private auth: AuthService, router: Router) {
-    auth.user$.subscribe(user => {
-      if(user) {
+  constructor(
+    private userService: UserService,
+    private auth: AuthService,
+    router: Router
+  ) {
+    auth.user$.subscribe((user) => {
+      if (user) {
+        userService.save(user);
         let returnUrl = localStorage.getItem('returnUrl');
-        if(returnUrl)
-        router.navigateByUrl(returnUrl);
+        if (returnUrl) router.navigateByUrl(returnUrl);
       }
     });
   }
